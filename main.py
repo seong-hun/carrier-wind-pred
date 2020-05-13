ec0757cc48067e596e5c3651c43f241d8036bcdd
Seong-hun Kim <kshoon92@gmail.com>
2020-05-13 19:35:25 +0900

    wip

---
+++ main.py
@@ -0,0 +1,172 @@
+import numpy as np
+from scipy import interpolate
+
+import tmc
+from tmc.terrain import Terrain
+
+
+class WindShear:
+    def __init__(self, vx, vy):
+        self.v = np.vstack((vx, vy, 0))
+
+    def get(self, pos):
+        return self.v
+
+
+class WindUpdraft:
+    """
+    Refereces:
+        [1] https://doi.org/10.1109/AIS.2010.5547048
+        [2] https://doi.org/10.2514/6.2006-1510
+
+    wast and zi are selected from Table.2 in [2], in July and maximum wast.
+        wast: 6.3, zi: 3962
+    """
+    ktable = np.array([
+        [1.5352, 2.5826, -0.0113, 0.0008],
+        [1.5265, 3.6054, -0.0176, 0.0005],
+        [1.4866, 4.8356, -0.0320, 0.0001],
+        [1.2042, 7.7904, 0.0848, 0.0001],
+        [0.8816, 13.9720, 0.3404, 0.0001],
+        [0.7067, 23.9940, 0.5689, 0.0002],
+        [0.6189, 42.7965, 0.7157, 0.0001],
+    ])
+    rratiotable = np.array([0.14, 0.25, 0.36, 0.47, 0.58, 0.69, 0.80])
+    kfun = interpolate.interp1d(rratiotable, ktable, axis=0)
+
+    def __init__(self, A, center=(0, 0), zi=3962, wast=6.3,
+                 basewind=None, terrain=None):
+        self.A = A
+        self.zi = zi
+        self.wast = wast
+        self.basewind = basewind
+        self.terrain = terrain
+        self.center = np.asarray(center)
+
+    def get(self, pos):
+        pos = pos.squeeze()
+        x, y = pos[:2]
+        h = -pos[2] - self._get_base_height(x, y)
+        xc, yc = self._get_center(h)
+        w = self._get_w(x, y, h, xc, yc, zi=self.zi, wast=self.wast, A=self.A)
+        return np.vstack((0, 0, -w))
+
+    def _get_base_height(self, x, y):
+        h = 0
+        if terrain:
+            h + terrain.get_height(x, y)
+        return h
+
+    def _get_center(self, z):
+        """z is altitude"""
+        ratio = 50 * np.tanh(12 * z / self.zi)
+        center = self.center
+        if self.basewind:
+            pos = np.vstack((0, 0, -z))
+            basevel = self.basewind.get(pos).squeeze()[:2]
+            center = center + basevel * ratio
+        return center
+
+    def _get_w(self, x, y, z, xc, yc, A, zi, wast):
+        """z is altitude"""
+        zratio = z / zi
+        r2 = max(10, 0.102 * zratio**(1/3) * (1 - 0.25 * zratio) * zi)
+        r1 = (0.0011 * r2 + 0.14 if r2 < 600 else 0.8) * r2
+        r = np.sqrt((x - xc)**2 + (y - yc)**2)
+        k1, k2, k3, k4 = self.kfun(r1 / r2)
+        N = 1
+
+        wbar = wast * zratio**(1/3) * (1 - 1.1 * zratio)
+        wpeak = wbar * 3 * r2**2 * (r2 - r1) / (r2**3 - r1**3)
+
+        is_zratio_in = zratio > 0.5 and zratio < 0.9
+
+        if r > r1 and r < 2 * r2 and is_zratio_in:
+            wD = wbar * 5 * np.pi / 12 * (zratio - 0.5) * np.sin(np.pi * r / r2)
+        else:
+            wD = 0
+
+        if is_zratio_in:
+            wemult = 1 - 2.5 * (zratio - 0.5)
+        else:
+            wemult = 1
+
+        we = - wbar * N * np.pi * r2**2 / (A - N * np.pi * r2**2) * wemult
+        w = (
+            1 / (1 + np.abs(k1 * r / r2 + k3)**k2)
+            + k4 * r / r2 + wD
+        ) * (wpeak - we) + we
+        return w
+
+
+# Map
+MAPSIZE = 500
+GRIDSIZE = 21
+xlim = (-MAPSIZE, MAPSIZE)
+ylim = (-MAPSIZE, MAPSIZE)
+xgrid = np.linspace(*xlim, GRIDSIZE)
+ygrid = np.linspace(*ylim, GRIDSIZE)
+xmap, ymap = np.meshgrid(xgrid, ygrid)
+
+terrain = Terrain()
+basewind = WindShear(6, 6)
+A = float(np.diff(xlim) * np.diff(ylim))
+wind = WindUpdraft(A=A, basewind=basewind, terrain=terrain)
+
+hlist = [10, 70, 140, 200]
+hmap = np.zeros_like(xmap)
+wmap = np.zeros((len(hlist),) + xmap.shape)
+for i, j in np.ndindex(xmap.shape):
+    x = xmap[i, j]
+    y = ymap[i, j]
+    terrain_h = terrain.get_height(x, y)
+    hmap[i, j] = terrain_h
+
+    for k, h in enumerate(hlist):
+        z = -terrain_h - h
+        pos = np.vstack((x, y, z))
+        wmap[k, i, j] = -wind.get(pos)[-1]
+
+
+# Meta Learning
+"""https://github.com/grey-eye/talking-heads"""
+raw_dataset = UpdraftDataset()
+dataset = DataLoader(raw_dataset, batch_size=BATCH_SIZE, shuffle=True)
+
+for epoch in range(EPOCHS):
+    for batch_num, (i, video) in enumerate(dataset):
+        # video [B, K+1, 2, C, W, H]
+
+        t = video[:, -1, ...]  # [B, 2, C, W, H]
+        video = video[:, :-1, ...]  # [B, K, 2, C, W, H]
+        dims = video.shape
+
+        # Calculate average encoding vector for video
+        e_in = video.reshape(dims[0] * dims[1], *dims[2:])  # [BxK, 2, C, W, H]
+        x, y = e_in[:, 0, ...], e_in[:, 1, ...]  # [BxK, C, W, H]
+        e_vectors = E(x, y).reshape(dims[0], dims[1], -1)  # [B, K, len(e)]
+        e_hat = e_vectors.mean(dim=1)
+
+
+# Plotting
+import matplotlib
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+plt.matshow(hmap, origin="lower")
+
+# 3D figure
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+for h, w in zip(hlist, wmap):
+    levels = np.linspace(w.min(), w.max(), 20)
+    w[np.logical_and(w > -0.6, w < 0.6)] = np.nan
+    w = h + w
+    ax.contourf(xmap, ymap, w, levels=h + levels)
+
+ax.set_xlim3d(*xlim)
+ax.set_ylim3d(*ylim)
+ax.set_zlim3d(0, max(hlist))
+
+plt.show()
